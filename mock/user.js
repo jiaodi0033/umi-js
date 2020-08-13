e6170c6434782000b8fd534780e777e65a00857b
jiaodi0033 <2322009884@qq.com>
2020-08-13 18:09:09 +0800

    1.0

--- mock/user.js
+++ mock/user.js
@@ -7,4 +7,25 @@ export default {
     code: 0,
     msg: '',
   },
+  'GET /api/v1/todolists': {
+    data: [
+      {
+        itemId: 8,
+        description: 'day4',
+        finished: false,
+      },
+      {
+        itemId: 9,
+        description: 'day5',
+        finished: false,
+      },
+      {
+        itemId: 10,
+        description: 'day6',
+        finished: false,
+      },
+    ],
+    code: 0,
+    msg: '',
+  },
 };
